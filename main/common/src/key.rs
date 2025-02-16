e70e3dfc66ce3dbc40d7568965f979ad45319ed5
Martinplior <martinpliorl@gmail.com>
2025-02-17 01:16:20 +0800

    Key::from_virtual_key改为查表实现

--- main/common/src/key.rs
+++ main/common/src/key.rs
@@ -139,122 +139,123 @@ impl Key {
     pub fn from_virtual_key(virtual_key: VIRTUAL_KEY, is_extend: bool) -> Self {
         use Key::*;
         use KeyboardAndMouse::*;
-        match virtual_key {
-            VK_ESCAPE => Escape,
-            VK_F1 => F1,
-            VK_F2 => F2,
-            VK_F3 => F3,
-            VK_F4 => F4,
-            VK_F5 => F5,
-            VK_F6 => F6,
-            VK_F7 => F7,
-            VK_F8 => F8,
-            VK_F9 => F9,
-            VK_F10 => F10,
-            VK_F11 => F11,
-            VK_F12 => F12,
-            VK_SNAPSHOT => PrintScreen,
-            VK_SCROLL => ScrollLock,
-            VK_PAUSE => Pause,
-            VK_OEM_3 => BackTick,
-            VK_1 => Key1,
-            VK_2 => Key2,
-            VK_3 => Key3,
-            VK_4 => Key4,
-            VK_5 => Key5,
-            VK_6 => Key6,
-            VK_7 => Key7,
-            VK_8 => Key8,
-            VK_9 => Key9,
-            VK_0 => Key0,
-            VK_OEM_MINUS => Minus,
-            VK_OEM_PLUS => Equal,
-            VK_BACK => Backspace,
-            VK_INSERT => Insert,
-            VK_DELETE => Delete,
-            VK_HOME => Home,
-            VK_END => End,
-            VK_PRIOR => PageUp,
-            VK_NEXT => PageDown,
-            VK_A => KeyA,
-            VK_B => KeyB,
-            VK_C => KeyC,
-            VK_D => KeyD,
-            VK_E => KeyE,
-            VK_F => KeyF,
-            VK_G => KeyG,
-            VK_H => KeyH,
-            VK_I => KeyI,
-            VK_J => KeyJ,
-            VK_K => KeyK,
-            VK_L => KeyL,
-            VK_M => KeyM,
-            VK_N => KeyN,
-            VK_O => KeyO,
-            VK_P => KeyP,
-            VK_Q => KeyQ,
-            VK_R => KeyR,
-            VK_S => KeyS,
-            VK_T => KeyT,
-            VK_U => KeyU,
-            VK_V => KeyV,
-            VK_W => KeyW,
-            VK_X => KeyX,
-            VK_Y => KeyY,
-            VK_Z => KeyZ,
-            VK_OEM_4 => LeftSquareBracket,
-            VK_OEM_6 => RightSquareBracket,
-            VK_OEM_5 => BackwardSlash,
-            VK_OEM_1 => Semicolon,
-            VK_OEM_7 => Apostrophe,
-            VK_OEM_COMMA => Comma,
-            VK_OEM_PERIOD => Period,
-            VK_OEM_2 => ForwardSlash,
-            VK_RETURN => match is_extend {
-                true => NumpadEnter,
-                false => Enter,
-            },
-            VK_CONTROL => match is_extend {
-                true => RightControl,
-                false => LeftControl,
-            },
-            VK_SHIFT => match is_extend {
-                true => RightShift,
-                false => LeftShift,
-            },
-            VK_MENU => match is_extend {
-                true => RightAlt,
-                false => LeftAlt,
-            },
-            VK_SPACE => Space,
-            VK_LWIN => LeftWin,
-            VK_RWIN => RightWin,
-            VK_APPS => Apps,
-            VK_TAB => Tab,
-            VK_CAPITAL => CapsLock,
-            VK_UP => Up,
-            VK_DOWN => Down,
-            VK_LEFT => Left,
-            VK_RIGHT => Right,
-            VK_NUMLOCK => NumLock,
-            VK_NUMPAD1 => Numpad1,
-            VK_NUMPAD2 => Numpad2,
-            VK_NUMPAD3 => Numpad3,
-            VK_NUMPAD4 => Numpad4,
-            VK_NUMPAD5 => Numpad5,
-            VK_NUMPAD6 => Numpad6,
-            VK_NUMPAD7 => Numpad7,
-            VK_NUMPAD8 => Numpad8,
-            VK_NUMPAD9 => Numpad9,
-            VK_NUMPAD0 => Numpad0,
-            VK_ADD => NumpadPlus,
-            VK_SUBTRACT => NumpadMinus,
-            VK_MULTIPLY => NumpadMultiply,
-            VK_DIVIDE => NumpadDivide,
-            VK_SEPARATOR => NumpadSeparator,
-            VK_DECIMAL => NumpadDot,
-            _ => Unknown,
-        }
+        const LUT: [Key; 0x200] = {
+            let mut lut = [Unknown; 0x200];
+            lut[VK_ESCAPE.0 as usize] = Escape;
+            lut[VK_F1.0 as usize] = F1;
+            lut[VK_F2.0 as usize] = F2;
+            lut[VK_F3.0 as usize] = F3;
+            lut[VK_F4.0 as usize] = F4;
+            lut[VK_F5.0 as usize] = F5;
+            lut[VK_F6.0 as usize] = F6;
+            lut[VK_F7.0 as usize] = F7;
+            lut[VK_F8.0 as usize] = F8;
+            lut[VK_F9.0 as usize] = F9;
+            lut[VK_F10.0 as usize] = F10;
+            lut[VK_F11.0 as usize] = F11;
+            lut[VK_F12.0 as usize] = F12;
+            lut[VK_SNAPSHOT.0 as usize] = PrintScreen;
+            lut[VK_SCROLL.0 as usize] = ScrollLock;
+            lut[VK_PAUSE.0 as usize] = Pause;
+            lut[VK_OEM_3.0 as usize] = BackTick;
+            lut[VK_1.0 as usize] = Key1;
+            lut[VK_2.0 as usize] = Key2;
+            lut[VK_3.0 as usize] = Key3;
+            lut[VK_4.0 as usize] = Key4;
+            lut[VK_5.0 as usize] = Key5;
+            lut[VK_6.0 as usize] = Key6;
+            lut[VK_7.0 as usize] = Key7;
+            lut[VK_8.0 as usize] = Key8;
+            lut[VK_9.0 as usize] = Key9;
+            lut[VK_0.0 as usize] = Key0;
+            lut[VK_OEM_MINUS.0 as usize] = Minus;
+            lut[VK_OEM_PLUS.0 as usize] = Equal;
+            lut[VK_BACK.0 as usize] = Backspace;
+            lut[VK_INSERT.0 as usize] = Insert;
+            lut[VK_DELETE.0 as usize] = Delete;
+            lut[VK_HOME.0 as usize] = Home;
+            lut[VK_END.0 as usize] = End;
+            lut[VK_PRIOR.0 as usize] = PageUp;
+            lut[VK_NEXT.0 as usize] = PageDown;
+            lut[VK_A.0 as usize] = KeyA;
+            lut[VK_B.0 as usize] = KeyB;
+            lut[VK_C.0 as usize] = KeyC;
+            lut[VK_D.0 as usize] = KeyD;
+            lut[VK_E.0 as usize] = KeyE;
+            lut[VK_F.0 as usize] = KeyF;
+            lut[VK_G.0 as usize] = KeyG;
+            lut[VK_H.0 as usize] = KeyH;
+            lut[VK_I.0 as usize] = KeyI;
+            lut[VK_J.0 as usize] = KeyJ;
+            lut[VK_K.0 as usize] = KeyK;
+            lut[VK_L.0 as usize] = KeyL;
+            lut[VK_M.0 as usize] = KeyM;
+            lut[VK_N.0 as usize] = KeyN;
+            lut[VK_O.0 as usize] = KeyO;
+            lut[VK_P.0 as usize] = KeyP;
+            lut[VK_Q.0 as usize] = KeyQ;
+            lut[VK_R.0 as usize] = KeyR;
+            lut[VK_S.0 as usize] = KeyS;
+            lut[VK_T.0 as usize] = KeyT;
+            lut[VK_U.0 as usize] = KeyU;
+            lut[VK_V.0 as usize] = KeyV;
+            lut[VK_W.0 as usize] = KeyW;
+            lut[VK_X.0 as usize] = KeyX;
+            lut[VK_Y.0 as usize] = KeyY;
+            lut[VK_Z.0 as usize] = KeyZ;
+            lut[VK_OEM_4.0 as usize] = LeftSquareBracket;
+            lut[VK_OEM_6.0 as usize] = RightSquareBracket;
+            lut[VK_OEM_5.0 as usize] = BackwardSlash;
+            lut[VK_OEM_1.0 as usize] = Semicolon;
+            lut[VK_OEM_7.0 as usize] = Apostrophe;
+            lut[VK_OEM_COMMA.0 as usize] = Comma;
+            lut[VK_OEM_PERIOD.0 as usize] = Period;
+            lut[VK_OEM_2.0 as usize] = ForwardSlash;
+            lut[VK_RETURN.0 as usize] = Enter;
+            lut[VK_RETURN.0 as usize + 0x100] = NumpadEnter;
+            lut[VK_CONTROL.0 as usize] = LeftControl;
+            lut[VK_CONTROL.0 as usize + 0x100] = RightControl;
+            lut[VK_SHIFT.0 as usize] = LeftShift;
+            lut[VK_SHIFT.0 as usize + 0x100] = RightShift;
+            lut[VK_MENU.0 as usize] = LeftAlt;
+            lut[VK_MENU.0 as usize + 0x100] = RightAlt;
+            lut[VK_LCONTROL.0 as usize] = LeftControl;
+            lut[VK_RCONTROL.0 as usize] = RightControl;
+            lut[VK_LSHIFT.0 as usize] = LeftShift;
+            lut[VK_RSHIFT.0 as usize] = RightShift;
+            lut[VK_LMENU.0 as usize] = LeftAlt;
+            lut[VK_RMENU.0 as usize] = RightAlt;
+            lut[VK_SPACE.0 as usize] = Space;
+            lut[VK_LWIN.0 as usize] = LeftWin;
+            lut[VK_RWIN.0 as usize] = RightWin;
+            lut[VK_APPS.0 as usize] = Apps;
+            lut[VK_TAB.0 as usize] = Tab;
+            lut[VK_CAPITAL.0 as usize] = CapsLock;
+            lut[VK_UP.0 as usize] = Up;
+            lut[VK_DOWN.0 as usize] = Down;
+            lut[VK_LEFT.0 as usize] = Left;
+            lut[VK_RIGHT.0 as usize] = Right;
+            lut[VK_NUMLOCK.0 as usize] = NumLock;
+            lut[VK_NUMPAD1.0 as usize] = Numpad1;
+            lut[VK_NUMPAD2.0 as usize] = Numpad2;
+            lut[VK_NUMPAD3.0 as usize] = Numpad3;
+            lut[VK_NUMPAD4.0 as usize] = Numpad4;
+            lut[VK_NUMPAD5.0 as usize] = Numpad5;
+            lut[VK_NUMPAD6.0 as usize] = Numpad6;
+            lut[VK_NUMPAD7.0 as usize] = Numpad7;
+            lut[VK_NUMPAD8.0 as usize] = Numpad8;
+            lut[VK_NUMPAD9.0 as usize] = Numpad9;
+            lut[VK_NUMPAD0.0 as usize] = Numpad0;
+            lut[VK_ADD.0 as usize] = NumpadPlus;
+            lut[VK_SUBTRACT.0 as usize] = NumpadMinus;
+            lut[VK_MULTIPLY.0 as usize] = NumpadMultiply;
+            lut[VK_DIVIDE.0 as usize] = NumpadDivide;
+            lut[VK_SEPARATOR.0 as usize] = NumpadSeparator;
+            lut[VK_DECIMAL.0 as usize] = NumpadDot;
+            lut
+        };
+        let index = virtual_key.0 as usize + ((is_extend as usize) << 8);
+        unsafe { *LUT.get_unchecked(index) }
     }
 
     pub fn to_virtual_key(self) -> VIRTUAL_KEY {
@@ -383,7 +384,8 @@ impl std::fmt::Display for Key {
 
 #[cfg(test)]
 mod tests {
-    use super::Key;
+
+    use super::*;
 
     #[test]
     fn t1() {
@@ -396,5 +398,25 @@ mod tests {
             .collect();
         vec.into_iter()
             .for_each(|vk| println!("{}", Key::from_virtual_key(vk, false)));
+    }
+
+    #[test]
+    fn t2() {
+        use KeyboardAndMouse::*;
+        [
+            (VK_RETURN, false),
+            (VK_RETURN, true),
+            (VK_CONTROL, false),
+            (VK_CONTROL, true),
+            (VK_SHIFT, false),
+            (VK_SHIFT, true),
+            (VK_MENU, false),
+            (VK_MENU, true),
+        ]
+        .into_iter()
+        .for_each(|(vk, is_extend)| {
+            let k = Key::from_virtual_key(vk, is_extend);
+            println!("{:?}", k);
+        });
     }
 }
